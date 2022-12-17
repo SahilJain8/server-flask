ec42f4eb6f4f53cf9a8d7adb76c7e15e35a61d36
Sahil <68115080+SahilJain8@users.noreply.github.com>
2022-12-17 15:28:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,9 @@ import warnings
 warnings.filterwarnings('ignore')
 
 app=Flask(__name__)
-app.config['SERVER_NAME'] = 'localhost:5000'
+
 
 app.secret_key = "secret key"
-
-
-
-
 
 @app.route("/")
 def index():
